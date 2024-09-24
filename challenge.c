d216dc0718cdcc01895350f12962fe3ab05132d4
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-09-24 16:23:42 +0100

    challenge1.c

---
+++ challenge.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int c;
+    printf("Saisir  la température en Celsius c = ")
+    scanf("%")
+    return 0;
+}
---
+++ challenge1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int gitmain()
+{
+    char a[15],b[15],d[30],e[5];
+    int r;
+    printf("Le nom : ");
+    scanf("%s",&a);
+    printf("\n");
+    printf("Le prenom : ");
+    scanf("%s",&b);
+    printf("\n");
+    printf("age : ");
+    scanf("%d",&r);
+    printf("\n");
+    printf("Le sexe : ");
+    scanf("%s",&e);
+    printf("\n");
+    printf("L'adresse email : ");
+    scanf("%s",&d);
+    return 0;
+} 
